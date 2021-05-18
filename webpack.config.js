13622564dfbf1994b04b3b15810005999911c290
ayoub-husein <ayoub1538367@gmail.com>
2021-05-18 20:33:26 +0300

    fonts and images

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,32 @@ module.exports ={
                 "sass-loader",
                 ],
             },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: "images",
+                    }
+                  }
+                ]
+            },
+
+            {
+                test: /\.(svg|eot|woff|woff2|ttf)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: "fonts",
+                    }
+                  }
+                ]
+            },
         ],
     },
 
